8accecb016994064565d68c1c27e5b8ea963316a
maxmurr <maxmurr.m@gmail.com>
2022-10-20 00:17:00 +0700

    add employee view and router

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import AllEmployee from '@views/Employees/AllEmployee.vue'
+import AllEmployeeView from '@/views/Employees/AllEmployeeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/employees',
+      path: 'employees',
       name: 'allEmployee',
-      component: AllEmployee
+      component: AllEmployeeView
     }
   ]
 })
--- app/src/views/Employees/AllEmployeeView.vue
+++ app/src/views/Employees/AllEmployeeView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h1> Hello Test </h1>
         <h3 class="text-xl">Name: {{ employees.name }}</h3>
         <p class="text-4xl">Position: {{ employees.position }}</p>
     </div>
