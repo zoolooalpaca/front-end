2caaf335a23c1cb3da899c60434533ed5709e0a3
napatswift <napat.do@ku.th>
2022-10-19 15:59:08 +0700

    set base url

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/order-in/'
-})
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      port: 3000,
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    base: mode === 'production' ? "/or/" : './',
+  };
+});
