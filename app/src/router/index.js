d3f880f3189ff062258a6f371c1cd49a36d7dfb4
maxmurr <maxmurr.m@gmail.com>
2022-10-19 23:36:39 +0700

    test get data from employee

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import AllEmployee from '@views/Employees/AllEmployee.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/employees',
+      name: 'allEmployee',
+      component: AllEmployee
+    }
   ]
 })
 
---
+++ app/src/views/Employees/AllEmployeeView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3 class="text-xl">Name: {{ employees.name }}</h3>
+        <p class="text-4xl">Position: {{ employees.position }}</p>
+    </div>
+</template>
