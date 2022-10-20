8ec2d128c49f961f1f35f73eb9c43cf41a842508
maxmurr <maxmurr.m@gmail.com>
2022-10-21 01:28:05 +0700

    table design

---
+++ app/src/components/Table/Table.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="inline-flex flex-col space-y-4 items-center justify-center bg-white"
+        style="width: 1032px; height: 526px;">
+        <div class="flex flex-col space-y-4 items-start justify-start" style="width: 1032px; height: 88px;">
+            <p class="text-3xl leading-10 text-black" style="width: 744px;">รับลูกค้าใหม่</p>
+            <p class="text-2xl font-medium leading-loose text-black" style="width: 744px;">เลือกโต๊ะ</p>
+        </div>
+        <div class="flex flex-col space-y-4 items-start justify-start" style="width: 744px; height: 422px;">
+            <div class="inline-flex space-x-4 items-start justify-between " style="width: 744px; height: 130px;">
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500 ">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">1</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">2</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">3</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">4</p>
+                </button>
+                <button class="flex items-center justify-center w-1/6 h-full bg-gray-900 bg-opacity-10 rounded-2xl">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">5</p>
+                </button>
+            </div>
+            <div class="inline-flex space-x-4 items-start justify-between" style="width: 744px; height: 130px;">
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">6</p>
+                </button>
+                <button class="flex items-center justify-center w-1/6 h-full bg-gray-900 bg-opacity-10 rounded-2xl">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">7</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">8</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">9</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">10</p>
+                </button>
+            </div>
+            <div class="inline-flex space-x-4 items-start justify-between" style="width: 744px; height: 130px;">
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">11</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">12</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">13</p>
+                </button>
+                <button
+                    class="flex items-center justify-center w-1/6 h-full bg-blue-100 rounded-2xl focus:outline-none focus:ring focus:ring-blue-500">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">14</p>
+                </button>
+                <button class="flex items-center justify-center w-1/6 h-full bg-gray-900 bg-opacity-10 rounded-2xl">
+                    <p class="text-base font-medium tracking-wide leading-normal text-gray-900">15</p>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
