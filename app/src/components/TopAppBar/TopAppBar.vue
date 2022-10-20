b19b5b2ace645339e70abd09dce3cbb360220e3e
maxmurr <maxmurr.m@gmail.com>
2022-10-21 02:05:46 +0700

    TopAppBar design

---
+++ app/src/components/TopAppBar/TopAppBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="inline-flex space-x-4 items-center justify-start px-3 pt-3 pb-3 bg-white"
+        style="width: 412px; height: 64px;">
+        <button class="rounded-full p-2 bg-orange-100">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+            </svg>
+        </button>
+        <p class="w-48 text-xl font-bold leading-7 text-gray-900">อาหารในถาด</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+svg {
+    color: black
+}
+</style>
