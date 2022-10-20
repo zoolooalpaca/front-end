df2c587767ddda1b4e3542b1dcf8bd51dcda09dd
napatswift <napat.do@ku.th>
2022-10-21 02:36:00 +0700

    Add eslint

---
+++ app/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'google',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
+
