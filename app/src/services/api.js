79b7e3b0cb78ea5cff92555721ba57a28755be4d
maxmurr <maxmurr.m@gmail.com>
2022-10-21 23:09:45 +0700

    lint

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://zoolooalpaca-api.loca.lt/api',
-});
+    baseURL: 'http://zoolooalpaca-api.loca.lt/api'
+})
 
 export const employeeAPI = {
-  async getAll() {
-    const response = await axiosInstance.get('/employees');
-    if (response.status === 200) {
-      return response.data;
+    async getAll() {
+        const response = await axiosInstance.get('/employees')
+        if (response.status === 200) {
+            return response.data.data
+        }
+        return []
     }
-    return [];
-  },
-};
+}
