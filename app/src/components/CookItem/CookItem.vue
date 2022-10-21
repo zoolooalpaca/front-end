dd4d2e338affbc6237be038abb05a21b928e05a1
coolfreewerx <78583528+coolfreewerx@users.noreply.github.com>
2022-10-21 22:46:27 +0700

    add cookItem

---
+++ app/src/components/CookItem/CookItem.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        <p class="text-xl secondary-text">{{ tableNumber }}</p>
+        <h1 class="text-6xl mt-4 .e-1">{{ foodName }} x{{ foodAmount }}</h1>
+        <h3 class="mt-4 tertiary-text">{{ foodDescription }}</h3>
+        <button block pill variant="primary">ทำ</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tableNumber : "โต๊ะ 1",
+            foodName : "ส้มตำไข่เค็ม",
+            foodDescription : "ส้มตำไข่เค็มอร่อยยั่ว ๆ ถูกปากคนไทย",
+            foodAmount : "1"
+        }
+    }
+}
+</script>
+
+<style>
+</style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <script>
 import NavBarDrawer from '@/components/NavBarDrawer/NavBarDrawer.vue';
+import CookItem from '@/components/CookItem/CookItem.vue';
 
 export default {
-  components: {NavBarDrawer},
+  components: 
+  {
+    NavBarDrawer,
+    CookItem,
+  },
 };
 </script>
 
 <template>
   <div>
     <NavBarDrawer></NavBarDrawer>
+    <CookItem></CookItem>
     <p class="surface-tint-text">surface-tint-text</p>
     <p class="surface-tint-color-text">surface-tint-color-text</p>
     <div class="error-container">
