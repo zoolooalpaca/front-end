690377937c9a1101ebe7a69a619c9aedc86b593d
napatswift <napat.do@ku.th>
2022-10-21 23:55:56 +0700

    Linting

--- app/src/components/TopAppBar/TopAppBar.vue
+++ app/src/components/TopAppBar/TopAppBar.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="inline-flex space-x-4 items-center justify-start px-3 pt-3 pb-3 bg-white">
-        <button class="rounded-full p-2 bg-orange-100">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-            </svg>
+    <div
+      class="inline-flex items-center
+      justify-start gap-2"
+    >
+        <button class="secondary-container w-8 h-8 rounded-full items-center">
+          <div class="flex items-center justify-center">
+            <span class="material-symbols-outlined">
+              arrow_back
+            </span>
+          </div>
         </button>
-        <p class="w-48 text-xl font-bold leading-7 text-gray-900">{{ label }}</p>
+        <p
+          class="w-48 text-xl font-bold leading-7
+          on-surface-text">
+          {{ label }}
+        </p>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            label: 'หัวข้อ',
-        };
-    },
+  data() {
+    return {
+      label: 'หัวข้อ',
+    };
+  },
 };
 </script>
 
 <style>
-svg {
-    color: black
-}
 </style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <script>
 import NavBarDrawer from '@/components/NavBarDrawer/NavBarDrawer.vue';
-import BillOrderItem from '../components/BillOrderItem/BillOrderItem.vue';
 import TableItem from '@/components/Table/TableItem.vue';
 import TopAppBar from '@/components/TopAppBar/TopAppBar.vue';
 
 
 export default {
-  components: {NavBarDrawer, BillOrderItem, TableItem, TopAppBar},
+  components: {NavBarDrawer, TableItem, TopAppBar},
 };
 </script>
 
@@ -14,7 +13,6 @@ export default {
   <div>
     <TopAppBar></TopAppBar>
     <NavBarDrawer></NavBarDrawer>
-    <BillOrderItem></BillOrderItem>
     <TableItem></TableItem>
     <p class="surface-tint-text">surface-tint-text</p>
     <p class="surface-tint-color-text">surface-tint-color-text</p>
