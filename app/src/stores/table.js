ba14110cfa247490e09389d39a1befc7e9b5803c
maxmurr <maxmurr.m@gmail.com>
2022-10-23 17:17:27 +0700

    get, save and delete table

---
+++ app/src/stores/table.js
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+import { tableAPI } from "@/services/api.js";
+
+export const useTableStore = defineStore({
+    id: "table",
+    state: () => {
+        return {
+            tables: [],
+        };
+    },
+    getter: {
+        getTables: (state) => state.tables,
+    },
+    actions: {
+        async fetch() {
+            this.tables = await tableAPI.getAll();
+        },
+
+        async save(table) {
+            const response = await tableAPI.saveNew(table);
+            if (response.success) {
+                this.tables.push(response);
+                return response.tableId;
+            }
+            return false;
+        },
+
+        delete(tableId) {
+            this.tables = this.tables.filter(
+                (table) => table.tableId !== tableId
+            );
+        }
+    },
+});
