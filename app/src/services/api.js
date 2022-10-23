49188b4b5f7901be4964c1162b14b04d61615e2e
maxmurr <maxmurr.m@gmail.com>
2022-10-24 03:20:09 +0700

    review api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -22,3 +22,53 @@ export const employeeAPI = {
     };
   },
 };
+
+export const reviewAPI = {
+  async getAll() {
+    const response = await axiosInstance.get('/reviews');
+    if (response.status === 200) {
+      return response.data;
+    }
+    return [];
+  },
+
+  async saveNew(review) {
+    const response = await axiosInstance.post('/reviews', review);
+    if (response.status === 201) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async update(review) {
+    const response = await axiosInstance.put('/reviews', review);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async get(id) {
+    const response = await axiosInstance.get(`/reviews/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async delete(id) {
+    const response = await axiosInstance.delete(`/reviews/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+};
