cba6efbfbca65e66b1fa783a3255240c7f7c7a13
maxmurr <maxmurr.m@gmail.com>
2022-10-24 03:56:54 +0700

    order api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -22,3 +22,47 @@ export const employeeAPI = {
     };
   },
 };
+
+export const orderApi = {
+  async getAll() {
+    const response = await axiosInstance.get('/orders');
+    if (response.status === 200) {
+      return response.data;
+    }
+    return [];
+  },
+
+  async saveNew(order) {
+    const response = await axiosInstance.post('/orders', order);
+    if (response.status === 201) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async update(order) {
+    const response = await axiosInstance.put(
+        `/orders/${order.order_number}`, order,
+    );
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async get(orderNumber) {
+    const response = await axiosInstance.get(
+        `/orders/${orderNumber}`,
+    );
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+};
