0a498341fec55326d1291f990c1bf5083f8ce1fb
maxmurr <maxmurr.m@gmail.com>
2022-10-24 03:28:17 +0700

    rating api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -22,3 +22,53 @@ export const employeeAPI = {
     };
   },
 };
+
+export const ratingAPI = {
+  async getAll() {
+    const response = await axiosInstance.get('/ratings');
+    if (response.status === 200) {
+      return response.data;
+    }
+    return [];
+  },
+
+  async saveNew(rating) {
+    const response = await axiosInstance.post('/ratings', rating);
+    if (response.status === 201) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async update(rating) {
+    const response = await axiosInstance.put('/ratings', rating);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async get(id) {
+    const response = await axiosInstance.get(`/ratings/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+
+  async delete(id) {
+    const response = await axiosInstance.delete(`/ratings/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+    return {
+      success: false,
+    };
+  },
+};
