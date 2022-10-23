1b2563b18703595e8970c8605e5e3e093fdb26b7
ManeesornV <maneesorn.v@ku.th>
2022-10-24 02:05:08 +0700

    update Components/FoodCard FoodCategoryChip PromotionBanner

---
+++ app/src/components/FoodCategoryChip/FoodCategoryChip.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+      class="bg-image"
+      style="
+      background-image: url('https://img.wongnai.com/p/1920x0/2017/06/22/bbf899f7ab4341dea4aec6330c2afafd.jpg');
+      border-radius: 20px;">
+    <div class="mask food-category-chip p-5" style="background-color: rgba(128, 128, 128, 0.5);">
+      <div class="d-flex justify-content-center align-items-center">
+        <p class="title-large text-center text-black">{{ name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      image : "https://img.wongnai.com/p/1920x0/2017/06/22/bbf899f7ab4341dea4aec6330c2afafd.jpg",
+      name : "อาหารไทย"
+    }
+  },
+  methods : {
+
+  }
+}
+</script>
+
+<style>
+.food-category-chip{
+  border-radius: 20px;
+}
+</style>
