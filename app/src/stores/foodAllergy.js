c12acc66d2a80909dc9569bbab0f240bf1ad3612
maxmurr <maxmurr.m@gmail.com>
2022-10-23 19:41:34 +0700

    update food_allergy name
    change name foodAllergies to food_allergies to match table in db

--- app/src/stores/foodAllergy.js
+++ app/src/stores/foodAllergy.js
@@ -1,34 +1,34 @@
-import { defineStore } from 'pinia';
-import { foodAllergyAPI } from '@/services/api.js';
+import {defineStore} from 'pinia';
+import {foodAllergyAPI} from '@/services/api.js';
 
 export const useFoodAllergyStore = defineStore({
-    id: 'foodAllergy',
-    state: () => {
-        return {
-            foodAllergies: [],
-        };
+  id: 'food_allergies',
+  state: () => {
+    return {
+      food_allergies: [],
+    };
+  },
+  getter: {
+    getFoodAllergies: (state) => state.food_allergies,
+  },
+  actions: {
+    async fetch() {
+      this.food_allergies = await foodAllergyAPI.getAll();
     },
-    getter: {
-        getFoodAllergies: (state) => state.foodAllergies,
-    },
-    actions: {
-        async fetch() {
-            this.foodAllergies = await foodAllergyAPI.getAll();
-        },
 
-        async save(foodAllergy) {
-            const response = await foodAllergyAPI.saveNew(foodAllergy);
-            if (response.success) {
-                this.foodAllergies.push(response);
-                return response.id;
-            }
-            return false;
-        },
-
-        delete(id) {
-            this.foodAllergies = this.foodAllergies.filter(
-                (foodAllergy) => foodAllergy.id !== id,
-            );
-        }
+    async save(foodAllergy) {
+      const response = await foodAllergyAPI.saveNew(foodAllergy);
+      if (response.success) {
+        this.foodAllergies.push(response);
+        return response.id;
+      }
+      return false;
     },
+
+    delete(id) {
+      this.food_allergies = this.foodAllergies.filter(
+          (foodAllergy) => foodAllergy.id !== id,
+      );
+    },
+  },
 });
