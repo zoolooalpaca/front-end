b5b604a0a57fccc6095c2f9626e1e0724b05e282
thatsawan2002 <thatsawan.c@ku.th>
2022-10-26 18:30:14 +0700

    update create review (not yet)

--- app/src/views/reviews/CreateReviewView.vue
+++ app/src/views/reviews/CreateReviewView.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="m-5">
+  <div class="m-5 block">
     <h1 class="text-3xl text-center">รีวิว</h1>
 
     <div>
       <label for="name">การให้บริการ</label>
-      <StarRating></StarRating>
+      <StarRating v-model="rating.count"></StarRating>
     </div>
 
     <div>
-      <label>รสชาติอาหาร</label>
-      <StarRating></StarRating>
+      <label for="name">รสชาติอาหาร</label>
+      <StarRating v-model="rating.count"></StarRating>
     </div>
 
     <div>
-      <label>ความสะอาด</label>
-      <StarRating></StarRating>
+      <label for="name">ความสะอาด</label>
+      <StarRating v-model="rating.count"></StarRating>
     </div>
 
     <div>
-      <label>ความรวดเร็ว/ความสะดวก</label>
-      <StarRating></StarRating>
+      <label for="name">ความรวดเร็ว/ความสะดวก</label>
+      <StarRating v-model="rating.count"></StarRating>
     </div>
 
-    <div>
+    <div class="flex flex-col">
       <label>ข้อเสนอแนะ</label>
-      <textarea name="" id="" cols="30" rows="10" v-model="review.feedback"></textarea>
+      <textarea name="" id="" cols="80" rows="10" v-model="review.feedback" type="text"
+                class="mt-3 border border-gray-700 rounded-lg bg-center"></textarea>
     </div>
 
     <div>
       <button @click="saveNewReview"
-              class="p-4 text-white bg-blue-900 border rounded">ส่ง</button>
+              class="p-3 mt-5 text-white bg-blue-900 border rounded-lg float-right">ส่ง</button>
     </div>
-
   </div>
 </template>
 
@@ -48,6 +48,7 @@ export default {
   data(){
     return{
       review: '',
+      rating: '',
       error: null
     }
   },
@@ -58,9 +59,13 @@ export default {
     async saveNewReview() {
       try {
         this.error = null
-        const review_id = this.review_store.save(this.review)
+        const review_id = await this.review_store.save(this.review)
+        const rating_id = await this.rating_store.save(this.rating)
         if (review_id){
-
+          this.$router.push(`/review/${review_id}`)
+        }
+        if (rating_id){
+          this.$router.push(`/rating/${rating_id}`)
         }
       }catch (error){
         console.log(error)
@@ -70,3 +75,7 @@ export default {
   }
 }
 </script>
+
+<style>
+
+</style>
