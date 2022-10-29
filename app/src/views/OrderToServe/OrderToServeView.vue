4046199ecb24bfb447ef0cdcfc04f942591922bd
Nittipak <nittipak.l@ku.th>
2022-10-29 13:02:50 +0700

    update

--- app/src/views/OrderToServe/OrderToServeView.vue
+++ app/src/views/OrderToServe/OrderToServeView.vue
@@ -1,29 +1,32 @@
 <template>
     <div class="relative">
-        <div>
-            <SectionHeader label="รายการอาหารที่ต้องเสิร์ฟ"></SectionHeader>
-            <div>
-                <label for="order_amount">{{orders}} รายการ</label>
+        <div class="flex flex-row">
+            <NavDrawer ></NavDrawer>
+            <div class="p-4 flex-grow">
+                <SectionHeader label="รายการอาหารที่ต้องเสิร์ฟ"></SectionHeader>
+                <div class="py-4 text-right flex-col">
+                    <label for="order_amount">{{orders}} รายการ</label>
+                </div>
+                    <div>
+                        <ToServeItem></ToServeItem>
+                        <ToServeDrawer></ToServeDrawer>
+                    </div>
+                </div>
             </div>
-            <div>
-                <ToServeItem></ToServeItem>
-                <ToServeDrawer></ToServeDrawer>
-            </div>
-        </div>
-    
-    </div>
-    
+    </div>   
 </template>
     
 <script>
     import SectionHeader from '../../components/NavBarDrawer/SectionHeader.vue';
     import ToServeItem from '../../components/ToServeItem/ToServeItem.vue';
     import ToServeDrawer from '../../components/ToServeItem/ToServeDrawer.vue';
+    import NavDrawer from '../../components/NavBarDrawer/NavBarDrawer.vue';
     export default{
         components:{
         SectionHeader,
         ToServeItem,
-        ToServeDrawer
+        ToServeDrawer,
+        NavDrawer
     },
 }
 </script>
