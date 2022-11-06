e34f3122b5af05cb64f50dd7e895b03b57be2358
Maxthomas MURRAY <maxthomas.m@ku.th>
2022-11-07 04:01:08 +0700

    Updated History View

--- app/src/views/OrderHistory/OrderHistoryView.vue
+++ app/src/views/OrderHistory/OrderHistoryView.vue
@@ -1,109 +1,122 @@
 <template>
-    <div class="relative">
-        <div>
-            <TopAppBar label="ประวัติ"></TopAppBar>
-        </div>
-        <div class="py-8">
-            <HistoryItem
-            v-for="order,i in orderHistoryList"
-            :id="order.id"
-            :status="order.order_status"
-            :foodImage="order.food_image.thumb"
-            :foodName="order.food_name"
-            :orderPrice="order.order_price"
-            :orderQuantity="order.order_quantity"
-            :orderRequest="order.order_request"
-            :onRemoveOrder="onRemoveOrder"
-            :key="i"
-                >
-            </HistoryItem>
-        </div>
-        <div class="fixed left-0 bottom-0 w-full p-4">
-            <FloatingHistoryOrder class="mx-10"
-                :totalOrder="orderItemCount"
-                :tprice="totalOrderPrice"
-            />
-        </div>
-    </div>
+  <div class="overflow-x-auto relative">
+
+      <div>
+          <TopAppBar label="ประวัติ"></TopAppBar>
+      </div>
+
+      <div class="py-8">
+          <HistoryItem
+          v-for="order in orders"
+          :key="order.id"
+          :status="order.status"
+          :foodImage="order.food_image"
+          :foodName="order.food_name"
+          :orderPrice="order.food_price"
+          :orderQuantity="order.order_quantity"
+          :orderRequest="order.order_request"
+          :onRemoveOrder="onRemoveOrder">
+          <h1>{{order.id}}</h1>
+          </HistoryItem>
+      </div>
+
+      
+
+      <div class="fixed left-0 bottom-0 w-full p-4">
+          <FloatingHistoryOrder class="mx-10" 
+              :totalOrder="orderItemCount"
+              :tprice="totalOrderPrice">
+          </FloatingHistoryOrder>
+      </div>
+
+  </div>
 </template>
 
 <script>
-import FloatingHistoryOrder from '../../components/FloatingHistoryOrder.vue';
+// import axios from 'axios';
+import {useOrderStore} from '@/stores/order.js';
 import HistoryItem from '../../components/HistoryItem/HistoryItem.vue';
 import TopAppBar from '../../components/TopAppBar/TopAppBar.vue';
-import { useOrderStore } from '../../stores/order';
+import FloatingHistoryOrder from '../../components/FloatingHistoryOrder.vue';
 
 export default {
   components:
-  {
-    TopAppBar,
-    HistoryItem,
-    FloatingHistoryOrder,
-  },
+{
+  TopAppBar,
+  HistoryItem,
+  FloatingHistoryOrder,
+},
 
-  setup(){
+  name: 'AllOrderView',
+  setup() {
     const orderStore = useOrderStore();
     return {orderStore};
   },
-
   data() {
     return {
-      orderedItems: [],
+      orders: [{
+          status: 'รอทำ',
+          food_image: 'https://i.ytimg.com/vi/YgmYqZWW4V8/maxresdefault.jpg',
+          food_name: 'ข้าวมันไก่',
+          food_price: '45',
+          order_quantity: '1',
+          order_request: 'ขอหนังล้วน ๆ ไม่เอาเนื้อไก่',
+        },
+        {
+          status: 'กำลังทำ',
+          food_image: 'https://i.ytimg.com/vi/YgmYqZWW4V8/maxresdefault.jpg',
+          food_name: 'ข้าวมันไก่',
+          food_price: '45',
+          order_quantity: '2',
+          order_request: 'ขอหนังล้วน ๆ ไม่เอาเนื้อไก่',
+        },
+        {
+          status: 'ส่งถึงโต๊ะแล้ว',
+          food_image: 'https://i.ytimg.com/vi/YgmYqZWW4V8/maxresdefault.jpg',
+          food_name: 'ข้าวมันไก่',
+          food_price: '45',
+          order_quantity: '3',
+          order_request: 'ขอหนังล้วน ๆ ไม่เอาเนื้อไก่',
+        },],
     };
   },
 
-  created() {
-    this.getOrderItems();
-  },
-
-  methods:{
-    async getOrderItems() {
+  methods: {
+    async getOrders() {
       await this.orderStore.fetch();
-      this.orderedItems = this.orderStore.orders.data;
-      console.log(this.orderedItems)
+      this.orders = this.orderStore.orders.data;
+    }, 
+    onRemoveOrder(id) {
+      this.orders = this.orders.filter((order) => order.id !== id);
     },
-    onRemoveOrder() {
-      this.orderedItems = this.orderStore.orders.data;
-    }
   },
 
+  // คำนวณจำนวนรายการที่สั่ง
   computed: {
-    orderHistoryList() {
-      const orderHistory = this.orderedItems.reduce(
-          (prev, curr) => [...prev, ...curr.order_description], []);
-      return orderHistory;
-    },
-
     orderItemCount() {
-      return this.orderedItems.reduce(
-        (prev, curr) =>
-          prev + (curr.order_description
-          ? curr.order_description.length
-          : 0), 0)
+      let count = [];
+      this.orders.forEach((order) => {
+        count += order.order_quantity
+      });
+      if (count == 0) {
+        return 0;
+      } else {
+        return count[count.length - 1];
+      }
     },
-
+    // คำนวณราคารวม
     totalOrderPrice() {
-      return this.orderedItems.reduce((prev, curr) => {
-          return prev + (curr.order_description
-          ? curr.order_description.reduce(
-            (prev, curr) => prev + (curr.order_price * curr.order_quantity), 0)
-          : 0
-        )}, 0)
+      let price = 0;
+      this.orders.forEach((order) => {
+        price += order.food_price * order.order_quantity;
+      });
+      return price;
     },
   },
-    
+
+  mounted() {
+    console.log('Order List Component Mounted');
+  },
+  
 };
-
 </script>
-
-<style>
-.cancel-button {
-    width: 90px;
-    height: 40px;
-    background: var( --md-sys-color-error);
-    color: var( --md-sys-color-on-primary);
-
-    /* outline */
-    border-radius: 100px;
-}
-</style>
